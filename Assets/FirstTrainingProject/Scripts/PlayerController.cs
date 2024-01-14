7aa152f1bbe54a8d7ee6b3e0eb28fac5e722a247
Vladislav Filatov <v.filatov@softchamber.ru>
2024-01-14 20:07:52 +0400

    Create player and paint the floor

--- Assets/FirstTrainingProject/Scripts/PlayerController.cs
+++ Assets/FirstTrainingProject/Scripts/PlayerController.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float _movementSpeed;
+    private float _movementSpeed; // 2
     [SerializeField]
-    private float _rotateSpeed;
+    private float _rotateSpeed; // 100
 
     void Start()
     {
-        
+        transform.position = new Vector3(1, 3, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * _movementSpeed);
-        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * _rotateSpeed);
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * _movementSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * _rotateSpeed * Time.deltaTime);
     }
 }
